a92c22e6601b909ca859e741b857cc9e92740ca6
Zakary Kurzawski <zakarykurzawski@Zakarys-MacBook-Pro.local>
2017-12-22 23:54:57 -0600

    added second view contorller for products collection view

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Zakary Kurzawski on 12/22/17.
+//  Copyright © 2017 Zakary Kurzawski. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
